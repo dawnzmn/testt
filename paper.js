e43ce31489e50635301d138455745d0c77754757
dawnzmn <83068262+dawnzmn@users.noreply.github.com>
2021-05-23 18:51:59 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,32 @@
+class paper {
+    constructor (x,y,r) {
+        var options={
+            setStatic: false,
+            restitution: 0.3,
+            density: 1.2,
+            friction: 0
+        }
+
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.image = loadImage("paper.png");
+    this.body = Bodies.circle(this.x,this.y,(this.r-20)/2,options);
+
+    World.add(world,this.body);
+    }
+
+    display(){
+        var paperPos = this.body.position;
+
+        push()
+        translate(paperPos.x,paperPos.y);
+        imageMode(CENTER);
+       // strokeWeight(5);
+       // fill("blue")
+       // ellipse(0,0,this.r,this.r);
+       image(this.image, 0, 0, this.r, this.r);
+        pop()
+    }
+
+}
